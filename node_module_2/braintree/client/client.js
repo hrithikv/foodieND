48b8e7bb534176ce46ef333b8f728f3d7b084952
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-10 09:21:02 -0400

    Update client.js

--- node_module_2/braintree/client/client.js
+++ node_module_2/braintree/client/client.js
@@ -180,39 +180,6 @@ Client.prototype._findOrCreateFraudnetJSON = function (clientMetadataId) {
 };
 
 /**
- * Used by other modules to formulate all network requests to the Braintree gateway. It is also capable of being used directly from your own form to tokenize credit card information. However, be sure to satisfy PCI compliance if you use direct card tokenization.
- * @public
- * @param {object} options Request options:
- * @param {string} options.method HTTP method, e.g. "get" or "post".
- * @param {string} options.endpoint Endpoint path, e.g. "payment_methods".
- * @param {object} options.data Data to send with the request.
- * @param {number} [options.timeout=60000] Set a timeout (in milliseconds) for the request.
- * @param {callback} [callback] The second argument, <code>data</code>, is the returned server data.
- * @example
- * <caption>Direct Credit Card Tokenization</caption>
- * var createClient = require('braintree-web/client').create;
- *
- * createClient({
- *   authorization: CLIENT_AUTHORIZATION
- * }, function (createErr, clientInstance) {
- *   var form = document.getElementById('my-form-id');
- *   var data = {
- *     creditCard: {
- *       number: form['cc-number'].value,
- *       cvv: form['cc-cvv'].value,
- *       expirationDate: form['cc-expiration-date'].value,
- *       billingAddress: {
- *         postalCode: form['cc-postal-code'].value
- *       },
- *       options: {
- *         validate: false
- *       }
- *     }
- *   };
- *
- *   // Warning: For a merchant to be eligible for the easiest level of PCI compliance (SAQ A),
- *   // payment fields cannot be hosted on your checkout page.
- *   // For an alternative to the following, use Hosted Fields.
  *   clientInstance.request({
  *     endpoint: 'payment_methods/credit_cards',
  *     method: 'post',
@@ -228,47 +195,6 @@ Client.prototype._findOrCreateFraudnetJSON = function (clientMetadataId) {
  * <caption>Tokenizing Fields for AVS Checks</caption>
  * var createClient = require('braintree-web/client').create;
  *
- * createClient({
- *   authorization: CLIENT_AUTHORIZATION
- * }, function (createErr, clientInstance) {
- *   var form = document.getElementById('my-form-id');
- *   var data = {
- *     creditCard: {
- *       number: form['cc-number'].value,
- *       cvv: form['cc-cvv'].value,
- *       expirationDate: form['cc-date'].value,
- *       // The billing address can be checked with AVS rules.
- *       // See: https://articles.braintreepayments.com/support/guides/fraud-tools/basic/avs-cvv-rules
- *       billingAddress: {
- *         postalCode: form['cc-postal-code'].value,
- *         streetAddress: form['cc-street-address'].value,
- *         countryName: form['cc-country-name'].value,
- *         countryCodeAlpha2: form['cc-country-alpha2'].value,
- *         countryCodeAlpha3: form['cc-country-alpha3'].value,
- *         countryCodeNumeric: form['cc-country-numeric'].value
- *       },
- *       options: {
- *         validate: false
- *       }
- *     }
- *   };
- *
- *   // Warning: For a merchant to be eligible for the easiest level of PCI compliance (SAQ A),
- *   // payment fields cannot be hosted on your checkout page.
- *   // For an alternative to the following, use Hosted Fields.
- *   clientInstance.request({
- *     endpoint: 'payment_methods/credit_cards',
- *     method: 'post',
- *     data: data
- *   }, function (requestErr, response) {
- *     // More detailed example of handling API errors: https://codepen.io/braintree/pen/MbwjdM
- *     if (requestErr) { throw new Error(requestErr); }
- *
- *     console.log('Got nonce:', response.creditCards[0].nonce);
- *   });
- * });
- * @returns {Promise|void} Returns a promise if no callback is provided.
- */
 Client.prototype.request = function (options, callback) {
   var self = this; // eslint-disable-line no-invalid-this
   var requestPromise = new Promise(function (resolve, reject) {
@@ -430,36 +356,11 @@ Client.prototype.toJSON = function () {
   return this.getConfiguration();
 };
 
-/**
- * Returns the Client version.
- * @public
- * @returns {String} The created client's version.
- * @example
- * var createClient = require('braintree-web/client').create;
- *
- * createClient({
- *   authorization: CLIENT_AUTHORIZATION
- * }, function (createErr, clientInstance) {
- *   console.log(clientInstance.getVersion()); // Ex: 1.0.0
- * });
- * @returns {void}
- */
+
 Client.prototype.getVersion = function () {
   return VERSION;
 };
 
-/**
- * Cleanly tear down anything set up by {@link module:braintree-web/client.create|create}.
- * @public
- * @param {callback} [callback] Called once teardown is complete. No data is returned if teardown completes successfully.
- * @example
- * clientInstance.teardown();
- * @example <caption>With callback</caption>
- * clientInstance.teardown(function () {
- *   // teardown is complete
- * });
- * @returns {Promise|void} Returns a promise if no callback is provided.
- */
 Client.prototype.teardown = wrapPromise(function () {
   var self = this; // eslint-disable-line no-invalid-this
 
